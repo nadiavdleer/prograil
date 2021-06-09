fad173e84ec0d1fdad52ae7f4b6895d0ee6e42e2
Sarah Danner <sarahdanner@MacBook-Air-van-Sarah.local>
2021-06-09 15:33:18 +0200

    add output csv file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from code.classes.trajectory import Trajectory
 from code.loaders.loader import load_holland
 from baseline import baseline
 from code.algorithms.quality import quality
-
+import csv
 
 if __name__ == "__main__":
     stations, connections = load_holland()
@@ -12,6 +12,11 @@ if __name__ == "__main__":
     trajectories = baseline(connections, stations)
 
     score = quality(connections, trajectories)
+
+    with open('output.csv', 'w') as file:
+        writer = csv.writer(file, delimiter=" ")
+        writer.writerow(["Trajecten:", len(trajectories)])
+        writer.writerow(["Score:", score])
 
     print("   o O___ _________")
     print(" _][__|o| |O O O O|")
