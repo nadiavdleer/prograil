5c17fa330c0b18441d0eddd65b889232399d8d42
nadiavdleer <nadiavdleer@gmail.com>
2021-06-18 10:14:17 +0200

    rewrite quality function

--- code/classes/timetable.py
+++ code/classes/timetable.py
@@ -5,29 +5,25 @@ class Timetable:
         self.traveled_connections = []
 
     def quality(self, connections):
-        p = 0
-        total_connections = 0
-        
-        time = 0 
-
-        for connection in connections:
-            total_connections += 1
-            if connection.traveled == True:
-                p += 1
-                time += connection.time
-        p = p / total_connections
-
+        used_connections = []
         T = 0
         Min = 0
+
         for trajectory in self.trajectories:
             T += 1
             Min += trajectory.total_time 
-            # print(trajectory)
-            # print(trajectory.total_time)
-    
-        
-        #print(f"p{p}")
-        #print(f"T{T}")
+            for connection in trajectory.connections:
+                used_connections.append(connection)
+
+        p = 0
+        total_connections = 0
+        for connection in connections:
+            total_connections += 1
+            if connection in used_connections:
+                p += 1
+                
+        p = p / total_connections
+
         K = p*10000 - (T*100 + Min)
         return K
 
